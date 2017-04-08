b702cd8c4acfb126e4049be01ba4e474733f14ce
jmfrolim <joao.mfreitas@live.com>
2017-04-08 09:38:44 -0300

    inicial commit

---
+++ One-BasicConcepts/ExtensionMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_BasicConcepts
+{
+   public static class ExtensionMethods
+    {
+        public static string LeftOf(this String src, string s)
+        {
+            string ret = src;
+            int idx = src.IndexOf(s);
+            if (idx != -1)
+            {
+                ret = src.Substring(0,idx);
+            }
+            return ret;
+        }
+
+        public static string RightOf(this String src,string s)
+        {
+            string ret = String.Empty;
+            int idx = src.IndexOf(s);
+
+            if (idx != -1)
+            {
+                ret = src.Substring(idx + s.Length);
+            }
+            return ret;
+        }
+    }
+}
---
+++ One-BasicConcepts/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace One_BasicConcepts
+{
+    class Program
+    {
+        static Semaphore semaforo;
+        static void Main(string[] args)
+        {
+            semaforo = new Semaphore(20, 20);
+            HttpListener listener = new HttpListener();
+            string url = "http://localhost/index.html";
+            listener.Prefixes.Add(url);
+            listener.Start();
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    semaforo.WaitOne();
+                    StartConnectionListener(listener);
+                }
+            });
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static async void StartConnectionListener(HttpListener listener)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            semaforo.Release();
+
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+
+            string path = request.RawUrl.LeftOf("?").RightOf("/");
+            Console.WriteLine(path);
+
+            try
+            {
+                string text = File.ReadAllText(path);
+                byte[] data = Encoding.UTF8.GetBytes(text);
+                response.ContentType = "text/html";
+                response.ContentLength64 = data.Length;
+                response.OutputStream.Write(data, 0, data.Length);
+                response.ContentEncoding = Encoding.UTF8;
+                response.StatusCode = 200;
+                response.OutputStream.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
